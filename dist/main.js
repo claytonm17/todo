f20de0fd4cbc143f4c79d5c8e667b3bc7767d9f3
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2023-12-13 12:32:39 -0500

    Create basic todo list functions in console

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("console.log('Happy Holidays!')\n\n//# sourceURL=webpack://09-todo/./src/index.js?");
+eval("// Todo list data manager\nconst toDoManager = (function () {\n\n    class ToDoItem {\n        constructor(title, description, dueDate, priority) {\n            this.title = title;\n            this.description = description;\n            this.dueDate = dueDate;\n            this.priority = priority;\n        }\n\n        log() {\n            console.log(`Title: ${this.title}`)\n            console.log(`Description: ${this.description}`)\n            console.log(`Due Date: ${this.dueDate}`)\n            console.log(`Priority: ${this.priority}`)\n        }\n    }\n\n    const toDoList = []\n    \n    function addNewItem(title, description, dueDate, priority) {\n        const newItem = new ToDoItem(title, description, dueDate, priority)\n        toDoList.push(newItem);\n    }\n\n    function removeItem(index) {\n        if (index >= 0 && index < toDoList.length) {\n            toDoList.splice(index, 1)\n        } else {\n            console.log('Invalid index')\n        }\n    }\n\n    function logList() {\n        for (let i = 0; i < toDoList.length; i++) {\n            console.log(`Task ${i + 1}`)\n            toDoList[i].log()\n            console.log('----------')\n        }\n    }\n\n\n    return {\n        addNewItem,\n        removeItem,\n        logList,\n    }\n})();\n\ntoDoManager.addNewItem('Homework', 'Chemistry reading', 'Friday', 'Medium');\ntoDoManager.addNewItem('Meeting', 'Meet with execs', 'Monday', 'High');\n\n\ntoDoManager.logList()\n\ntoDoManager.removeItem(0)\n\ntoDoManager.logList()\n\n//# sourceURL=webpack://09-todo/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
-console.log('Happy Holidays!')
+// Todo list data manager
+const toDoManager = (function () {
+
+    class ToDoItem {
+        constructor(title, description, dueDate, priority) {
+            this.title = title;
+            this.description = description;
+            this.dueDate = dueDate;
+            this.priority = priority;
+        }
+
+        log() {
+            console.log(`Title: ${this.title}`)
+            console.log(`Description: ${this.description}`)
+            console.log(`Due Date: ${this.dueDate}`)
+            console.log(`Priority: ${this.priority}`)
+        }
+    }
+
+    const toDoList = []
+    
+    function addNewItem(title, description, dueDate, priority) {
+        const newItem = new ToDoItem(title, description, dueDate, priority)
+        toDoList.push(newItem);
+    }
+
+    function removeItem(index) {
+        if (index >= 0 && index < toDoList.length) {
+            toDoList.splice(index, 1)
+        } else {
+            console.log('Invalid index')
+        }
+    }
+
+    function logList() {
+        for (let i = 0; i < toDoList.length; i++) {
+            console.log(`Task ${i + 1}`)
+            toDoList[i].log()
+            console.log('----------')
+        }
+    }
+
+
+    return {
+        addNewItem,
+        removeItem,
+        logList,
+    }
+})();
+
+toDoManager.addNewItem('Homework', 'Chemistry reading', 'Friday', 'Medium');
+toDoManager.addNewItem('Meeting', 'Meet with execs', 'Monday', 'High');
+
+
+toDoManager.logList()
+
+toDoManager.removeItem(0)
+
+toDoManager.logList()
